1ed8667dc3d2830a846482c00c71a407d00803af
amannanda200514361 <200514361@student.georgianc.on.ca>
2023-07-20 13:31:09 -0700

    Create Get-IPConfigurationReport.ps1
    just created a new script for week 10

---
+++ powershell/Get-IPConfigurationReport.ps1
@@ -0,0 +1,17 @@
+﻿# Get a collection of network adapter configuration objects
+$networkAdapters = Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled }
+
+# Create a custom object for each adapter with the required properties
+$reportData = $networkAdapters | ForEach-Object {
+    [PSCustomObject]@{
+        'Adapter Description' = $_.Description
+        'Index' = $_.Index
+        'IP Address' = $_.IPAddress -join ', '
+        'Subnet Mask' = $_.IPSubnet -join ', '
+        'DNS Domain Name' = $_.DNSDomain
+        'DNS Server' = $_.DNSServerSearchOrder -join ', '
+    }
+}
+
+# Format and display the report as a table
+$reportData | Format-Table -AutoSize
